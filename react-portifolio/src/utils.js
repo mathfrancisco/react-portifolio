5c923f236bdac5d0e3543c07ad9c8e4bdc460b5c
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-04-05 20:26:49 -0300

    Update utils.js

--- react-portifolio/src/utils.js
+++ react-portifolio/src/utils.js
@@ -1,4 +1,8 @@
-
+/**
+ * Função para obter o endereço de uma imagem.
+ * @param {string} path - O nome da imagem.
+ * @returns {string} O endereço da imagem.
+ */
 export const getImageUrl = (path) => {
   return new URL(`./assets/${path}`, import.meta.url).href;
 };
