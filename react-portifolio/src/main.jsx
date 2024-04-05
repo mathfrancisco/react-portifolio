0cfd6e045685b97bcb303db6e8fc7c20eb48d774
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-04-05 19:27:12 -0300

    Update main.jsx

--- react-portifolio/src/main.jsx
+++ react-portifolio/src/main.jsx
@@ -1,11 +1,17 @@
+// Importa as bibliotecas React e ReactDOM
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Importa o componente principal da aplicação
 import App from './app.jsx'
+// Importa o arquivo CSS principal da aplicação
 import './index.css'
 
+// Importa as fontes utilizadas na aplicação
 import "@fontsource/outfit"
 import "@fontsource/roboto"
 
+// Cria uma "raiz" React no elemento HTML com id "root" e renderiza o componente App dentro da "raiz" em modo estrito
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
